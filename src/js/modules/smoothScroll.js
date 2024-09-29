7d78b62bb8eb3684203e249ced5130cbc2d2e947
ArtNick <artem.nikonov.2005@bk.ru>
2024-09-29 23:51:37 +0500

    первая готовая версия

---
+++ src/js/modules/smoothScroll.js
@@ -0,0 +1,12 @@
+//с сайта https://www.jsdelivr.com/package/npm/smooth-scrolling
+// npm install smooth-scrolling
+/*
+
+const smooth = new Smooth({
+    native: true,
+    section: document.querySelector('.wrapper'),
+    ease: .1
+})
+
+smooth.init()
+*/
